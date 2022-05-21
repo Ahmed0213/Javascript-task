083dae183516d5a890f13ab657ff6f8861b0aa6f
Ahmed0213 <95652985+Ahmed0213@users.noreply.github.com>
2022-05-21 09:18:34 +0400

    JV-1

---
+++ Javascript-1/app.js
@@ -0,0 +1,51 @@
+// let myName= 'Ali';//define
+// let myAge = 22; //assign
+// myName=45;//declare
+// alert(myName)
+// alert(myAge)
+
+// let sum= 12 + 34;
+// alert(sum)
+
+// let name = prompt("adin nedir")
+// let age = prompt("nece yasin var")
+// alert (name + age)
+
+
+
+
+
+//TASK-1 IS READY
+// let width;
+// let height;
+//  height=prompt("enter height")
+// width = prompt("enter width")
+// area = confirm (width * height)
+
+//TASK-2 is Ready
+// let age;
+// let year;
+// year = prompt("enter your birth year");
+// age = 2022-year;
+// alert(age)
+
+//TASK-3
+let km;
+let mil;
+
+km= prompt("enter km for converting mile")
+mil= km * 0.6214;
+alert(mil)
+
+
+
+
+
+
+
+// prompt() //optionaldir
+// confirm("Gel gedek istirahete")
+
+//to assign name = "Ali"
+// optional ixtiuaridir
+// required vacib demekdir
