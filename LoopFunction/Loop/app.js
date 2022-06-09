6599df26f274655a54ff91dfd1765c60c7bbe2ce
Ahmed0213 <95652985+Ahmed0213@users.noreply.github.com>
2022-06-09 11:44:57 +0400

    task

---
+++ LoopFunction/Loop/app.js
@@ -0,0 +1,9 @@
+let n = parseInt(prompt("enter numner"))
+let i;
+
+for( i= 0; i < n; i++  ) {
+   
+   if (i % 5 == 0) {
+       console.log(i)
+   }
+}
--- LoopFunction/app.js
+++
@@ -1,8 +0,0 @@
-let n = parseInt(prompt("enter numner"))
-let i;
-
-for( i== 0; i < n; i++  ) {
-    if ( n % 5) {
-        console.log(n);
-    }
-}
