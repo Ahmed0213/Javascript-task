f09aa25604299ab0da2c3bfe8f0d5a68fc26f1c6
Ahmed0213 <95652985+Ahmed0213@users.noreply.github.com>
2022-06-25 12:11:23 +0400

    Update app.js

--- LoopFunction/Function/app.js
+++ LoopFunction/Function/app.js
@@ -4,13 +4,13 @@ function calculator() {
     let operator= prompt('Enter operator ( either +, -, * or / ): ')
     let number1= parseInt(prompt("Enter first number"))
     let number2= parseInt(prompt("Enter second number"))
-    if (operator = "+") {
+    if (operator == '+') {
         console.log(number1 + number2)
     }
-    else if (operator = "-") {
+    else if (operator == '-') {
         console.log(number1 - number2);
     }
-    else if (operator = "*") {
+    else if (operator == '*') {
         console.log(number1 * number2);
     }
     else {
